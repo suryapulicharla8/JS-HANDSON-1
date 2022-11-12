3a98ef4935930e18782db1346600c2305b2f3eac
suryapulicharla8 <suryapulicharla8@gmail.com>
2022-11-12 20:53:53 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+// a=10;
+// b=20;
+// sum=a+b;
+// console.log(sum/2);
+let var1=20;
+    b=20;
+    var1
+    console.log(var1*b)
+     
+    var a=100;
+    var b=4;
+    if(a%2===0 &&b%2===0){
+        console.log("you got it")
+    } else{
+        console.log("change number")
+    }
+// if if else if
+// var a=50;
+//     b=24;
+//     c=40;
+//     if(a>b &&a>c){
+//         console.log('A is the big')
+//     }else{
+//         console.log('b is big')
+//     }
+
+// a=90;
+// b=80;
+// c=70;
+// average=a+b+c/100*2
+// console.log(average)
+
+// var1=35363
+// var2=676838
+// diff=var1-var2
+// console.log(diff)
+
+// var1=5467
+// var2=34
+// console.log(var1%var2)
+//  var1=4567;
+//  console.log(var1+var1);
+// var a=50;
+//     b=20;
+//     c=100;
+//     sum=a+b+c;
+//     division=sum/3;
+    // console.log(a+b+c);
+    // console.log(division);
+    // console.log(a+b+c/3);
+
+    // console.log(a%b);
+    const First_Digit = (n) => {
+    var a=4567/1000;
+    var b=parseInt(a);
+    return b;
+   };
+ 
+
+        
+
